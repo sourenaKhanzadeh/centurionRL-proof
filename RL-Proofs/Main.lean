aac293f1ece9b243e81f03db6679853055b6d22d
sourena <sourena.khanzadeh@gmail.com>
2025-02-14 13:37:27 -0500

    Genereated a Lean Proj

---
+++ RL-Proofs/Main.lean
@@ -0,0 +1,4 @@
+import RLProofs
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
--- RL-Proofs/proof1.lean
+++ RL-Proofs/proof1.lean
@@ -1,1 +1,4 @@
 def add1 (n : Nat) : Nat := n + 1
+
+
+def volume (l : Nat) (w : Nat) (h : Nat) : Nat := l * w * h
